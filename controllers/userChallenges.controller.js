b3700dc50e74a4314044db7f998455dfae314d63
Rodrigo Otero <roterom82@gmail.com>
2019-04-02 10:28:17 +0200

    populate by userId on userChallenges.List

--- controllers/userChallenges.controller.js
+++ controllers/userChallenges.controller.js
@@ -3,7 +3,10 @@ const UserChallenge = require('../models/userChallenge.model');
 //const Evidence = require('../models/evidence.model');
 
 module.exports.list = (req, res, next) => {
+  console.log("ENTRO EN LIST");
   UserChallenge.find({isFinished: true})
+    .populate('challengeId')
+    .populate('userId')
     .populate('evidences')
     .then(userChallenges => res.status(200).json(userChallenges))
     .catch(next)
